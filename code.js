195315a991614b91aebfd9170dc1dc256ec19315
austin <austinhuang1203@gmail.com>
2022-09-02 22:22:26 +0800

    added green color

--- code.js
+++ code.js
@@ -1,7 +1,13 @@
 //variables
 let drawing1 = 0;
+
+// 0 is draw, 1 is stop
 let drawing2 = 1;
+
+// 0 is inactive, 1 is eraser on
 let eraser1 = 0;
+// 0  black, 1 blue, 2 green, 3 red
+let color = 0;
 
 
 //functions
@@ -17,26 +23,37 @@ function reset(){
     }
 }
 
+//most impt thing
 function change(x){
     const box1 = document.getElementById(`box${x}`);
     if(drawing2 == 0 && eraser1 ==0){
-    if(box1.className == "box1b"){
-    } else{
-        box1.className="box1b"
-    };
-    }else if(drawing2 == 0 && eraser1 == 1){
-        if(box1.className == "box1b"){
-            box1.className="box1w";
-        } else{
-        };
+        
+        if(color == 0){
+            box1.className="box1b"
+        }
+        else if (color == 2){
+            box1.className="box1g"
+        }
+            
+        
+
+    }
+    else if(drawing2 == 0 && eraser1 == 1){
+        
+        box1.className="box1w";
+         
     }
 
 }
 
 function eraser(){
     if (eraser1 == 0){
+        const eraserbtn = document.getElementById("button2");
+        eraserbtn.className = "button2"
         eraser1 = 1;
     } else{
+        const eraserbtn1 = document.getElementById("button2");
+        eraserbtn1.className = "button1"
         eraser1 = 0;
     };
 }
@@ -53,8 +70,32 @@ function toggle2(){
     }
 }
 
+const colorbtn = document.getElementsByClassName("colorbtn");
+
+function makegreen(){
+    color = 2;
+    for (let i = 0, long1 = colorbtn.length; i<long1; i++){
+        colorbtn[i].className = "button1 colorbtn"
+    };
+    const greenbtn = document.getElementById("button3");
+    greenbtn.className = "button2 colorbtn";
+}
+
+function makeblack(){
+    color = 0;
+    for (let i = 0, long1 = colorbtn.length; i<long1; i++){
+        colorbtn[i].className = "button1 colorbtn"
+    };
+    colorbtn.class = "button1 colorbtn"
+    const blackbtn = document.getElementById("button4");
+    blackbtn.className = "button2 colorbtn";
+}
+
+
+
 //run at start
 make();
+makeblack();
 
 //event listerners
 const box1 = document.getElementsByClassName("box1w");
@@ -70,3 +111,9 @@ btn1.addEventListener("click",reset);
 
 const btn2 = document.getElementById("button2");
 btn2.addEventListener("click",eraser);
+
+const btn3 = document.getElementById("button3");
+btn3.addEventListener("click", makegreen);
+
+const btn4 = document.getElementById("button4");
+btn4.addEventListener("click", makeblack);
